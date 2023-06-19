0012441398259546599f9ac4e7394363eceac5c4
Ewa Serafin.Belza <ewa.serafin.programista@gmail.com>
2023-06-19 22:51:23 +0200

    Add library

--- main.cpp
+++ main.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <limits>
+#include <cstdlib>
+#include <windows.h>
+#include <sstream>
+#include <cstdio>
+#include <set>
+#include<algorithm>
 
+using namespace std;
